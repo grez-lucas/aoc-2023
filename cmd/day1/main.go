12f8efc18d265630717566a26a0b053cb2af42be
grez-lucas <lucasgrez3010@gmail.com>
2024-10-19 12:39:13 -0300

    chore(fmt) Update format

--- cmd/day1/main.go
+++ cmd/day1/main.go
@@ -1,35 +1,34 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "bufio"
-  "github.com/grez-lucas/aoc-2023/utils"
+	"bufio"
+	"fmt"
+	"github.com/grez-lucas/aoc-2023/utils"
+	"os"
 )
 
 func main() {
-  file, err := os.Open("./input.txt")
-  
-  utils.CheckError(err)
-  defer file.Close() // Make sure the file closes regardless of how main() ends
+	file, err := os.Open("./input.txt")
 
-  scanner := bufio.NewScanner(file)
-  var totalSum int = 0
+	utils.CheckError(err)
+	defer file.Close() // Make sure the file closes regardless of how main() ends
 
+	scanner := bufio.NewScanner(file)
+	var totalSum int = 0
 
-  for scanner.Scan() {
-    line := scanner.Text()
-    err := scanner.Err()
-    utils.CheckError(err)
+	for scanner.Scan() {
+		line := scanner.Text()
+		err := scanner.Err()
+		utils.CheckError(err)
 
-    // Combine line's border digits and add them to the total sum
-    res, err := utils.CombineBorderDigits(line)
-    utils.CheckError(err)
+		// Combine line's border digits and add them to the total sum
+		res, err := utils.CombineBorderDigits(line)
+		utils.CheckError(err)
 
-    totalSum+=res
-  }
+		totalSum += res
+	}
 
-  utils.CheckError(err)
+	utils.CheckError(err)
 
-  fmt.Println("Result: ", totalSum)
+	fmt.Println("Result: ", totalSum)
 }
